444c8bcc965248f0a30140d1d6f09d0d4daaeabc
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-04 23:41:19 +0530

    coomngfjgnfdjkg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import { Component } from './Component'
 
 function App() {
+  const [name, setname] = useState('first')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>{name}</h1>
+        <button onClick={() => setname('second')}>Click</button>
+
+        <h1>{`Hello=  ${200 + 800}`}</h1>
+        <h1>Hello {name}</h1>
+
+        {/* <img
+          src={logo}
+          className='App-logo'
+          alt='logo'
+        />
+        <p>Kiran's First App</p>
+        <h1>Hello world</h1>
+        <h1>Heading</h1>
+        <h2 className='heading-style'>Heeading 2</h2>
+        <h3 className='heading-3'>Heading 3</h3>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
