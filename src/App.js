c164813c70ce389ee1f735147803d0e5ac6978c1
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-05 00:27:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg'
 import './App.css'
 import { useState } from 'react'
 import { Component } from './Component'
@@ -14,24 +13,6 @@ function App() {
 
         <h1>{`Hello=  ${200 + 800}`}</h1>
         <h1>Hello {name}</h1>
-
-        {/* <img
-          src={logo}
-          className='App-logo'
-          alt='logo'
-        />
-        <p>Kiran's First App</p>
-        <h1>Hello world</h1>
-        <h1>Heading</h1>
-        <h2 className='heading-style'>Heeading 2</h2>
-        <h3 className='heading-3'>Heading 3</h3>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a> */}
       </header>
     </div>
   )
